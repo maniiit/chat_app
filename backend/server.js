825377656bdda13450b62806a3b78276e1419f09
Manit Mallesh <manitmallesh1@gmail.com>
2025-01-16 20:52:40 -0600

    signup api implemented, still facing issues

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,17 @@
-const express = require("express");
-const app = express();
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./DB/connectToMongoDB.js";
 
-app.listen(5002, () => console.log("Server running on port 5000"));
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json()); //to parse incoming requests with JSON payloads (from req.body)
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server running on port ${PORT}`);
+});
